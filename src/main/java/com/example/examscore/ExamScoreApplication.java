88c6ce38af4e2d9b306230fb035da983288ff5ea
ThnhHuynh04 <22521406@gm.uit.edu.vn>
2025-07-14 15:21:38 +0700

    init

---
+++ src/main/java/com/example/examscore/ExamScoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.examscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamScoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamScoreApplication.class, args);
+    }
+
+}
